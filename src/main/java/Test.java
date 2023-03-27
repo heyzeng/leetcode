8f3d51adc3b3ee5cf76a2c3d8b481aea19802edc
heyzeng <270806204@qq.com>
2023-03-27 09:12:33 +0800

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Author:Jude
+ * Date:2023-03-27 上午9:11
+ */
+public class Test {
+    public static void print() {
+        System.out.println("test");
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
