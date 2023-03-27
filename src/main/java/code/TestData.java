3a419396c23328693e78e658015a9b72a647b01a
heyzeng <270806204@qq.com>
2023-03-27 11:16:19 +0800

    optimize code

--- src/main/java/code/TestData.java
+++ src/main/java/code/TestData.java
@@ -10,7 +10,7 @@ public class TestData {
     public static void main(String[] args) {
         //
         int[] nums = {1,2,5,7};
-        int target = 9;
+        int target = 10;
         LC_0001_TwoSum lc_0001_twoSum = new LC_0001_TwoSum();
         System.out.println(Arrays.toString(lc_0001_twoSum.twoSum(nums,target)));
 
